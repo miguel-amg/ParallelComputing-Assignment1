118c4a95a0495845d1b18a41ed262bd69be6cc40
Pedro Carneiro <pedrocarneiro1109@gmail.com>
2024-10-08 19:13:25 +0100

    Remocao do recalculo dos indices do array da lin_solve

--- 3dfluid-main/fluid_solver.cpp
+++ 3dfluid-main/fluid_solver.cpp
@@ -56,14 +56,19 @@ void set_bnd(int M, int N, int O, int b, float *x) {
 // Linear solve for implicit methods (diffusion)
 void lin_solve(int M, int N, int O, int b, float *x, float *x0, float a,
                float c) {
+  int M2=M+2; 
+  int N2=N+2;
+  int M2xN2=M2*N2;
   for (int l = 0; l < LINEARSOLVERTIMES; l++) {
     for (int i = 1; i <= M; i++) {
       for (int j = 1; j <= N; j++) {
+        int ixj=M2*j+i;
         for (int k = 1; k <= O; k++) {
-          x[IX(i, j, k)] = (x0[IX(i, j, k)] +
-                            a * (x[IX(i - 1, j, k)] + x[IX(i + 1, j, k)] +
-                                 x[IX(i, j - 1, k)] + x[IX(i, j + 1, k)] +
-                                 x[IX(i, j, k - 1)] + x[IX(i, j, k + 1)])) /
+          int ixjxk= ixj + M2xN2 * k;
+          x[ixjxk] = (x0[ixjxk] +
+                            a * (x[ixjxk-1] + x[ixjxk+1] +
+                                 x[ixjxk-M2] + x[ixjxk+M2] +
+                                 x[ixjxk-M2xN2] + x[ixjxk+M2xN2])) /
                            c;
         }
       }
